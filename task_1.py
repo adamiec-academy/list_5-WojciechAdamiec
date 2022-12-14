1bf4f174d02585497ae8e193b5da1eb4c777d27b
Wojciech Adamiec <wojciechadamiec99@gmail.com>
2022-12-14 16:00:38 +0100

    Implement solutions

--- task_1.py
+++ task_1.py
@@ -1,2 +1,10 @@
 def longest_word():
-    return
+    longest_word = None
+    longest_length = 0
+    for line in open("words.txt"):
+        word = line.strip()
+        word_len = len(word)
+        if word_len > longest_length:
+            longest_length = word_len
+            longest_word = word
+    return longest_word
--- task_4.py
+++ task_4.py
@@ -1,2 +1,18 @@
 def reversed_words():
-    return []
+    result = []
+
+    words = set()
+
+    for word in open("words.txt", encoding="utf-8"):
+        words.add(word.strip())
+
+
+    for word in words:
+        rev_word = word[::-1]
+        if rev_word in words and rev_word != word:
+            result.append(tuple(sorted((word, rev_word))))
+
+
+    result = list(set(result))
+    result.sort()
+    return result
